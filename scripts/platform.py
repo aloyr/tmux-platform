037849b50b9c9a8ae06aa61c92d7e309e5df8c0f
Peter Carrero <peter.carrero@gmail.com>
2024-12-30 13:01:52 -0600

    Add login check for platform cli

--- scripts/platform.py
+++ scripts/platform.py
@@ -22,12 +22,22 @@ def make_menu(projects: list[list[str]])-> None:
     p = Popen(cmd, stdout=PIPE)
     (data, err) = p.communicate()
 
+def login()-> bool:
+    Popen(['tmux','display-message','determining login status'], stdout=PIPE).communicate()
+    (stdout, stderr) = Popen(['platform', 'auth:info'], stdout=PIPE, stderr=PIPE).communicate()
+    if len(stderr) > 0:
+        Popen(['tmux','display-message','-d', '0', 'Error: Not logged in. Please login with "platfom auth:browser-login" first'], stdout=PIPE).communicate()
+        return False
+    return True
+
+
 def main()-> None:
-    Popen(['tmux','display-message','loading project list'], stdout=PIPE).communicate()
-    data = get_projects()
-    make_menu(data)
-    (selection, err) = Popen(['pbpaste'], stdout=PIPE).communicate()
-    Popen(['tmux','display-message','project id: ' + selection.decode()], stdout=PIPE).communicate()
+    if login():
+        Popen(['tmux','display-message','loading project list'], stdout=PIPE).communicate()
+        data = get_projects()
+        make_menu(data)
+        (selection, err) = Popen(['pbpaste'], stdout=PIPE).communicate()
+        Popen(['tmux','display-message','-d', '0', 'project id: ' + selection.decode() + ' copied to pasteboard'], stdout=PIPE).communicate()
 
 if __name__ == '__main__':
     main()
